dfda77ce90e88dea913417f5faf06edb60995ef1
muhammadzulfans <muhammadzulfans932@gmail.com>
2023-10-02 13:29:30 +0700

    test

--- src/project/aplikasi/cek/harga/barang/ProjectAplikasiCekHargaBarang.java
+++ src/project/aplikasi/cek/harga/barang/ProjectAplikasiCekHargaBarang.java
@@ -14,6 +14,7 @@ public class ProjectAplikasiCekHargaBarang {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Tes");
         // TODO code application logic here
     }
     
