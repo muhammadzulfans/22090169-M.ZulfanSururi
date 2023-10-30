9801c7c6a07911b93fc50d460595c59f095a3e5f
muhammadzulfans <muhammadzulfans932@gmail.com>
2023-10-30 22:49:46 +0700

    progres UAS

--- src/project/aplikasi/cek/harga/barang/ProjectAplikasiCekHargaBarang.java
+++ src/project/aplikasi/cek/harga/barang/ProjectAplikasiCekHargaBarang.java
@@ -1,21 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package project.aplikasi.cek.harga.barang;
 
-/**
- *
- * @author Lenovo
- */
 public class ProjectAplikasiCekHargaBarang {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Cek Harga Barang dengan QR Code");
-        // TODO code application logic here
+        System.out.println("data barang");
+        
     }
     
 }
